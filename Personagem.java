5183ee178f6cbd61f3ea340d9bb5ce940280e520
Davi Araújo <daviaraujo3078@gmail.com>
2023-03-09 16:26:27 -0300

    apostila02(oo): bloco de codigo 2.3.1 encerrado

--- Personagem.java
+++ Personagem.java
@@ -1,21 +1,37 @@
+import static java.lang.Math.min;
 public class Personagem{
     //variável de instância
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
     //isso é um método instância
     void cacar(){
+    if(energia >= 2){
         System.out.println(nome + " cacando");
+        energia -= 2;
     }
-
+    else{
+        System.out.println(nome + " sem energia para cacar");
+    }
+    fome = min(fome + 1, 10);
+    sono = min(sono + 1, 10);
+}
     void comer(){
+        if(fome >= 1){
         System.out.print(nome + " comendo\n");
+        energia = min (energia + 1, 10);
+        fome -= 1; // fome = fome - 1
+        }
+        else{
+            System.out.println(nome + "sem fome");
+        }
     }
 
     void dormir(){
-        System.out.printf("%s dormindo%n", nome);
+        if( sono >= 1){
+            System.out.printf("%s dormindo%n", nome);
+            energia = energia + 1 < 10 ? energia + 1 : 10;
+        }
     }
-
-
 }
